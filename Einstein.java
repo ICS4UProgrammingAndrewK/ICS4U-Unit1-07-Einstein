92fecb367f0374851d7f426d882bd067bfc1095d
Ubuntu <ubuntu@ip-172-31-19-228.ec2.internal>
2020-10-14 17:43:17 +0000

    Complete

---
+++ Einstein.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/**
+ * Class Einstein
+ */
+public class Einstein {
+	public static void main(String[] args) {
+    
+    Scanner userInput = new Scanner(System.in);
+    
+    //diclare the varible
+    double mass;
+    double speedOfLight = 2.998 * Math.pow(10, 8);
+    double energy;
+    
+    //this ask the user to insert the mass(Kg)
+    System.out.println(" Enter the mass of object in Kilograms:");
+    	
+    //This qual the mass to the user input
+    mass = userInput.nextDouble();
+    	
+    //Calculate the mass in to energy
+    mass = (mass * 2.998 * Math.pow(10, 8));
+    	
+    // Display the total energy
+    System.out.println(" The Total energy is : " + mass);
+    	
+	}
+}
